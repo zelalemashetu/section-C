bf8666fa8464dc6bc80fbfa7472d42a465b45d7f
zelalemashetu <zelalemashetu809@gmail.com>
2025-03-28 15:28:29 +0300

    Create swapcase.py

---
+++ ETS1507_15/string_methods/swapcase/swapcase.py
@@ -0,0 +1,6 @@
+
+print("Hello World".swapcase())  # Output: "hELLO wORLD"
+print("PyThOn".swapcase())       # Output: "pYtHoN"
+print("123ABCdef".swapcase())    # Output: "123abcDEF"
+print("hELLO!".swapcase())       # Output: "Hello!"
+
