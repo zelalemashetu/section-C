d184167296ecd4966fc369f2ea7413cc0a58dd4c
zelalemashetu <zelalemashetu809@gmail.com>
2025-04-06 10:41:54 +0300

    Create README.py

---
+++ ETS1507_15/string_methods/title/README.py
@@ -0,0 +1,7 @@
+# Definition and Usage
+# The title() method returns a string where the first character in every word is upper case. Like a header, or a title.
+
+# If the word contains a number or a symbol, the first letter after that will be converted to upper case.
+
+# Syntax
+# string.title()
