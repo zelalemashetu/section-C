9fb0094c7b8a251343cf51521d6ebef5e9aa35d9
zelalemashetu <zelalemashetu809@gmail.com>
2025-04-06 10:27:43 +0300

    Create is_alnum.py

---
+++ ETS1507_15/string_methods/is_alnum/is_alnum.py
@@ -0,0 +1,9 @@
+txt = "Company12"
+
+x = txt.isalnum() # returns true since all charaters are alphanumeric
+
+print(x)
+txt2='long time!' # return false since there is non alphanumeric in the string '!'
+print(txt2.isalnum())
+
+
