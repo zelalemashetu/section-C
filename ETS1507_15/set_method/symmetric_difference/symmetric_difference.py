231a7c2363412af91d6d838ef158b2bd2d0437f0
zelalemashetu <zelalemashetu809@gmail.com>
2025-04-13 10:14:42 +0300

    Create symmetric_difference.py

---
+++ ETS1507_15/set_method/symmetric_difference/symmetric_difference.py
@@ -0,0 +1,6 @@
+x = {"pen", "book", "note"}
+y = {"fox", "lion", "ox","pen"}
+
+z = x.symmetric_difference(y) #returns elements that are not found in both
+
+print(z) # the out put is combination of the all except "pen"
