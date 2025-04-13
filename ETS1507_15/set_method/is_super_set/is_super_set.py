8783461498a41579f9070f22629d9caca3b09543
zelalemashetu <zelalemashetu809@gmail.com>
2025-04-13 10:21:19 +0300

    Create is_super_set.py

---
+++ ETS1507_15/set_method/is_super_set/is_super_set.py
@@ -0,0 +1,7 @@
+x = {"f", "e", "d", "c", "b", "a"}
+y = {"a", "b", "c"}
+
+z = x.issuperset(y) # returns True if all itemsinspecified set exists in the original set.
+
+
+print(z) # the out put will be True.
